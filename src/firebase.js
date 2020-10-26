d60608afa62e85817eba5b95b84883405e8557f2
Varun <50090823+varun-official@users.noreply.github.com>
2020-10-26 21:58:56 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBp1k_us2vXVlxkmljDMv1V6gyPJUzndUw",
-  authDomain: "discord-clone-db97e.firebaseapp.com",
-  databaseURL: "https://discord-clone-db97e.firebaseio.com",
-  projectId: "discord-clone-db97e",
-  storageBucket: "discord-clone-db97e.appspot.com",
-  messagingSenderId: "548780290669",
-  appId: "1:548780290669:web:57fb49c3a37d180ec54564",
-  measurementId: "G-V0YF70LP9Y",
+/* YOUR CONFIG FILE */
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
